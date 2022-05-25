3a0307686274f518b3d550805db4de96ea192cf3
fatiudeen <shehufatiudeen@gmail.com>
2022-05-25 09:16:41 +0100

    .

--- src/api/v2/helpers/requestParser.js
+++ src/api/v2/helpers/requestParser.js
@@ -1,32 +1,32 @@
 export default {
-    parseRequest: (req) => {
-      let data = {};
-      let path = [];
+  parseRequest: (req) => {
+    let data = {};
+    let path = [];
 
-      if (req.files) {
-        req.files.map((file) => {
-          path.push(file.path);
-        });
-      }
-      req.body.alias ? (data.alias = req.body.alias) : false;
-      req.body.to ? (data.to = req.body.to) : false;
-      req.body.reference ? (data.reference = req.body.reference) : false;
-      req.body.title ? (data.title = req.body.title) : false;
-      data.from = req.taskType === 1 ? req.user.department : req.user._id;
-      data.message = {
-        body: req.body.text,
-        attachment: path,
-      };
-      return data;
-    },
-    parseMetadata: (req) => {
-      let data = {};
-      req.body.comment ? (data.comment = req.body.comment) : false;
-      data.user = req.user;
-      req.params.userId ? (data.id = req.params.userId) : false;
-      data.requestId = req.params.requestId;
-      req.params.status ? (data.status = req.params.status) : false;
-
-      return data;
-    },
+    if (req.files) {
+      req.files.map((file) => {
+        path.push(file.path);
+      });
+    }
+    req.body.alias ? (data.alias = req.body.alias) : false;
+    req.body.to ? (data.to = req.body.to) : false;
+    req.body.reference ? (data.reference = req.body.reference) : false;
+    req.body.title ? (data.title = req.body.title) : false;
+    data.from = req.taskType === 1 ? req.user.department : req.user._id;
+    data.message = {
+      body: req.body.text,
+      attachment: path,
+    };
+    return data;
   },
+  parseMetadata: (req) => {
+    let data = {};
+    req.body.comment ? (data.comment = req.body.comment) : false;
+    data.user = req.user;
+    req.params.userId ? (data.id = req.params.userId) : false;
+    data.requestId = req.params.requestId;
+    req.params.status ? (data.status = req.params.status) : false;
+
+    return data;
+  },
+};
